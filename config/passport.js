41ab2c81ed55e85014c0162e58f6d47680888433
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-05-07 19:08:06 -0500

    merging
    no conflicts

--- config/passport.js
+++ config/passport.js
@@ -10,23 +10,23 @@ passport.use(
     {
       usernameField: "email"
     },
-    function(email, password, done) {
+    function (email, password, done) {
       // When a user tries to sign in this code runs
       db.User.findOne({
         where: {
-          email: email
-        }
-      }).then(function(dbUser) {
+          email: email,
+        },
+      }).then(function (dbUser) {
         // If there's no user with the given email
         if (!dbUser) {
           return done(null, false, {
-            message: "Incorrect email."
+            message: "Incorrect email.",
           });
         }
         // If there is a user with the given email, but the password the user gives us is incorrect
         else if (!dbUser.validPassword(password)) {
           return done(null, false, {
-            message: "Incorrect password."
+            message: "Incorrect password.",
           });
         }
         // If none of the above, return the user
@@ -39,11 +39,11 @@ passport.use(
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
 
